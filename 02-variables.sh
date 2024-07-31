5fbf20d5b883a88ca7ceb304a5ab2f8d7c9fc1d2
asutraye <asutraye@ASUTRAYE-T490.in.oracle.com>
2024-07-31 17:30:22 +0530

    This is my second script to pring conversation between Anil & Skanda

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-echo "Anil: Hello Skanda, How are doing?"
-echo "Skanda: Hello Anil, I am doing good, How about you Anil"
-echo "Anil: I am also doing good, Thank you Skanda"
+PERSON1=Anil
+PERSON2=Skanda
+
+echo "$PERSON1: Hello Skanda, How are doing?"
+echo "$PERSON2: Hello $PERSON1, I am doing good, How about you $PERSON1"
+echo "$PERSON1: I am also doing good, Thank you $PERSON2"
