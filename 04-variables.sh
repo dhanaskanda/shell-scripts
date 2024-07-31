dec8277ddf6e8bddabbb749d5ff6f21b6ff50bfe
asutraye <asutraye@ASUTRAYE-T490.in.oracle.com>
2024-07-31 18:11:25 +0530

    Priting the date with variable

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,6 +3,6 @@
 PERSON1=$1
 PERSON2=$2
 
-echo "$PERSON1: Hello Skanda, How are doing?"
+echo "$PERSON1: Hello $PERSON2, How are doing?"
 echo "$PERSON2: Hello $PERSON1, I am doing good, How about you $PERSON1"
 echo "$PERSON1: I am also doing good, Thank you $PERSON2"
