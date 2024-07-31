40f03630d1afdd3b65625a1523aa7d4f932d3f48
asutraye <asutraye@ASUTRAYE-T490.in.oracle.com>
2024-07-31 19:15:54 +0530

    Array script to understand the outcome

---
+++ 07-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+COUNTRIES=(INDIA, SRINLANKA, AUSTRALIA, AMERICA)
+
+echo "My country is:: ${COUNTRIES[0]}"
+echo "Second country is:: ${COUNTRIES[1]}"
+echo "Thired country is:: ${COUNTRIES[2]}"
+echo "White country is:: ${COUNTRIES[3]}"
+
+echo "All countries are:: ${COUNTRIES[@]}"
