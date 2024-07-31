e42987930a022ca249f575515b799cadc9c4ad9c
asutraye <asutraye@ASUTRAYE-T490.in.oracle.com>
2024-07-31 18:33:08 +0530

    Printing the username on terminal

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 echo "Please enter your username"
-
 read -s USERNAME # the value entered above will be automatically attached to the USERNAME variable
-echo "Username is: $USERNAME"
+
+echo "Please enter your password"
+read -s PASSWORD
+echo "Username is: $USERNAME, Password is: $PASSWORD"  #Just to display the username and password but it is not best practice
+
