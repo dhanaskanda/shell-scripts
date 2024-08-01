77a6b428336ae0bf84fa12bf487e2fc825608969
asutraye <asutraye@ASUTRAYE-T490.in.oracle.com>
2024-08-01 21:48:05 +0530

    This script is to install mysql without any issues

---
+++ 11-logs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#!/bin/bash
+
+ID=$(id -u)
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+
+LOGSFILE=/tmp/$0-$TIMESTAMP.log
+
+CHECK(){
+    if [ $1 -ne 0 ]
+then
+    echo " $2 ....FAILED"
+    exit 1
+else
+    echo " $2 SUCCESS"
+fi
+}
+if [ $ID -ne 0 ]
+then
+    echo "Error: run the script using root user"
+    exit 1
+else
+    echo "You are root user"
+fi
+
+yum install mysql -y &>> $LOGSFILE
+
+CHECK $? "Installing MYSQL"
+
+yum install git -y &>> $LOGSFILE
+CHECK $? "Insalling GIT"
