7521342b2c516933b919bb4ee63ced97940db181
asutraye <asutraye@ASUTRAYE-T490.in.oracle.com>
2024-08-02 12:17:50 +0530

    shell scripts

---
+++ sample.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGSFILE="/tmp/$0-$TIMESTAMP.log"
+
+R=\e[31m
+G=\e[32m
+Y=\e[33m
+N=\e[0m
+
+echo -e "Script started execution at $Y $TIMESTAMP $N"
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R Error $N: Run the script from root user"
+    exit 1 # Exit if not 0
+else
+    echo -e "You are $G root $N user"
+fi
