d7375917d932889abd32a296a3936ab8e9b6d92c
asutraye <asutraye@ASUTRAYE-T490.in.oracle.com>
2024-08-06 20:07:37 +0530

    Getting disk details using shell script

---
+++ Disk-usage.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DISKUSAGE=$(df -hT | grep -vE 'tmp|File')
+DISK_THRESHOLD=1
+message=""
+
+while IFS= read line
+do
+    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+    partition=$(echo $line | awk '{print $1F}')
+    if [ $usage -ge $DISK_THRESHOLD ]
+    then
+        message+="echo High Disk usage on $partition: $usage"
+done <<< $DISKUSAGE
