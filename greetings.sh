5b89b6e4db9a288cd4f7805bd5cd0817bd662031
asutraye <asutraye@ASUTRAYE-T490.in.oracle.com>
2024-08-07 15:07:58 +0530

    disk utilization monitoring

--- greetings.sh
+++ greetings.sh
@@ -7,8 +7,8 @@ WISHES=""
 
 #---------Function------------#
 USAGE(){
-    echo "echo USAGE:: $(basename $0) -n <name> -w <wishes>"
-    echo "options::"
+    echo " USAGE:: $(basename $0) -n <name> -w <wishes>"
+    echo "Options::"
     echo " -n, Specify the name (mandatory)"
     echo " -w, Specify the wishes, ex Hi Hello Good morning, Good Evening"
     echo " -h, dispaly help and exit"
@@ -19,8 +19,8 @@ USAGE(){
 while getopts ":n:w:h" opt; do
     case $opt in
         n) NAME="$OPTARG";;
-        w) WISHES="$OPTORG";;
-        \?) echo "Invalid options: -"$OPTARG"" >&2; exit;;
+        w) WISHES="$OPTARG";;
+        \?) echo "Invalid options: -"$OPTARG"" >&2; USAGE; exit;;
         h) USAGE; exit;;
         :) USAGE; exit;;
     esac
