e5b56f81b0be23ba504a4f93f42c1e38a7d3a98f
asutraye <asutraye@ASUTRAYE-T490.in.oracle.com>
2024-08-07 15:27:42 +0530

    shell scripts

--- greetings.sh
+++ greetings.sh
@@ -3,14 +3,14 @@
 #----------Variables----------#
 
 NAME=""
-WISHES=""
+WISHES="Good Morning"
 
 #---------Function------------#
 USAGE(){
     echo " USAGE:: $(basename $0) -n <name> -w <wishes>"
     echo "Options::"
     echo " -n, Specify the name (mandatory)"
-    echo " -w, Specify the wishes, ex Hi Hello Good morning, Good Evening"
+    echo " -w, Specify the wishes (Optional). Default is good morning"
     echo " -h, dispaly help and exit"
 }
 
@@ -26,8 +26,10 @@ while getopts ":n:w:h" opt; do
     esac
 done
 
-if [ -z "$NAME" ] || [ -z "$WISHES" ]; then
-    echo "Error: Both -n and -w are mandatory options."
+#if [ -z "$NAME" ] || [ -z "$WISHES" ]; then
+if [ -z "$NAME" ]; then # Now wishes are optional
+    #echo "Error: Both -n and -w are mandatory options."
+    echo "Error: -n is mandatory option."
     USAGE
     exit 1
 fi
