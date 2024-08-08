ef63571e05e646c9581e4a6c9ee8ee4fe8fda94f
asutraye <asutraye@ASUTRAYE-T490.in.oracle.com>
2024-08-08 10:04:34 +0530

    Script to get the help and delete/archive the logs

--- getopts.sh
+++ getopts.sh
@@ -8,20 +8,21 @@ ACTION=""
 DEST_DIR=""
 MEMORY=""
 
-R=[31e
+R="\e[31m"
+N="\e[0m"
 
 USAGE(){
-    echo " USAGE:: $(basename $0) -s <source directory> -d <destination directory> -t <days> -m <memory> -a <action>"
+    echo " USAGE:: $(basename $0) -s <source directory> -d <destination directory> -t <Number of days> -m <memory size of file> -a <action:delete or archive>"
     echo "Options::"
     echo " -a, Specify the action is mandatory"
     echo " -s, Specify the source directory"
     echo " -m, Specify the memory"
-    echo " -d, Specify the destination directory"
-    echo " -t, Specify the days(optional) default is 14 days"
+    echo " -d, Specify the destination directory whic -a is archive"
+    echo " -t, Specify the days(optional) to delete the files.. default is 14 days"
     echo " -h, Display the help and exit"
 }
 
-while getopts ":-s:a:d:t:m" opt
+while getopts ":s:a:d:t:m" opt
 do
     case $opt in
         s) SOURCE_DIR="$OPTARG";;
@@ -50,9 +51,31 @@ then
     exit 1
 fi
 
-if [ ! -ne $SOURCE_DIR ]
+if [ ! -ne $SOURCE_DIR ] # Denotes opposite
 then
-    echo -e "$SOURCE_DIR $R does not exist $N"
+    echo -e "Source Directory: $SOURCE_DIR $R does not exist $N"
+fi
+
+#-------------------------DELETE/ARCHIVE ACTION----------------------#
+
+if [ Action == "delete" ]
+then
+    FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +"$DAYS" -name *.log)
+
+    while IFS= read -r line
+    do
+        echo "Deleting file: $line"
+        rm -rf $line
+    done <<< $FILES_TO_DELETE
+else
+    FILES_TO_ARCHIVE=$(find $SOURCE_DIR -type f -mtime +"$DAYS" -name "*.log")
+
+    while IFS= read -r line
+    do
+        echo "Archiving the files: $line"
+        zip -r "$DEST_DIR/$(basname "$line")".zip
+        rm -rf $line
+    done <<< $FILES_TO_ARCHIVE
 fi
 
 
