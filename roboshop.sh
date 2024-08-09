ab50f2d7bc2559fca4a5a18c219c4a3eceed080a
asutraye <asutraye@ASUTRAYE-T490.in.oracle.com>
2024-08-09 10:54:14 +0530

    AWS instance creation using CLI commands with shell script

--- roboshop.sh
+++ roboshop.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-AMI="ami-0b4f379183e5706b9"
+AMI_ID="ami-0b4f379183e5706b9"
 SG_ID="sg-0a9760d01e0335af7"
+DOMAIN_NAME="dhanaskanda.online"
+HOSTED_ZONE="Z067782025J385S1OCSZM"
 
 INSTANCES=("mongodb" "mysql" "redis" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 
@@ -14,8 +16,29 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    IPADDRESS=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-0a9760d01e0335af7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    IPADDRESS=$(aws ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids sg-0a9760d01e0335af7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
 
     echo "instance is: $i: $IPADDRESS"
 
+    # Creates route 53 records based on env name
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $HOSTED_ZONE \
+    --change-batch
+    {
+        {
+        "Comment": "Testing creating a record set"
+        ,"Changes": [{
+        "Action"              : "CREATE"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$i'.'$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'" $IPADDRESS "'"
+            }]
+        }
+        }]
+    }
+    '
 done
