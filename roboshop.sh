0cec188ac5d73e6758da0b95c808309512274a29
asutraye <asutraye@ASUTRAYE-T490.in.oracle.com>
2024-08-09 08:56:16 +0530

    AWS instance creation using CLI commands with shell script

--- roboshop.sh
+++ roboshop.sh
@@ -14,7 +14,7 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-0a9760d01e0335af7
+    aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-0a9760d01e0335af7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
 
     echo "instance is: $i"
 
